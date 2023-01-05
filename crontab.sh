49f68e116714f6ff5d46bdc664635573ac753ea4
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-01-05 13:58:42 +0300

    Update crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -2,6 +2,7 @@
 cat /dev/null > /var/spool/cron/root
 cat >>/var/spool/cron/root<<EOF
 # 40mins-interval
-0,40 0-22/2 * * * /root/Rotation.sh > /root/Rotation_log.txt
-20 1-23/2 * * * /root/Rotation.sh > /root/Rotation_log.txt
+#0,40 0-22/2 * * * /root/Rotation.sh > /root/Rotation_log.txt
+#20 1-23/2 * * * /root/Rotation.sh > /root/Rotation_log.txt
+59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
 EOF
