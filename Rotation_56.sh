65b3458e18c4adbc480bf7c0d73726042659b5e9
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-01-11 06:53:26 +0300

    Update Rotation_56.sh

--- Rotation_56.sh
+++ Rotation_56.sh
@@ -8,16 +8,11 @@ array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
         ip64() {
                 echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+        }
+	ip56() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
 	}
-        ip56() {
-                echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-        }       
-        if [ $Prefix -eq 56 ]
-        then
-                echo "$1$(ip56):$(ip64):$(ip64):$(ip64):$(ip64)"
-        else
-                echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)
-        fi
+	echo "$1$(ip56):$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
 gen_3proxy() {
