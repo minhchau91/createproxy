7a77994e9c74b609bafc8b0191ac718c01787c7a
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-01-11 06:31:43 +0300

    Update Nocix_CreateProxy.sh

--- Nocix_CreateProxy.sh
+++ Nocix_CreateProxy.sh
@@ -166,7 +166,7 @@ bash /etc/rc.local
 
 gen_proxy_file_for_user
 
-wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/Rotation.sh" --output-document=/root/Nocix_Rotation.sh
+wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/Rotation.sh" --output-document=/root/Rotation.sh
 chmod 777 /root/Rotation.sh
 cat >>/var/spool/cron/root<<EOF
 #day - time
