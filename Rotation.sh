c7cc75a21445a2ab1b7b4e2dc68b68b53d5e8f2c
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-01-13 07:55:06 +0300

    Update Rotation.sh

--- Rotation.sh
+++ Rotation.sh
@@ -1,43 +1,15 @@
 #!/bin/sh
 random() {
-	tr </dev/urandom -dc A-Za-z0-9 | head -c5
-	echo
+        tr </dev/urandom -dc A-Za-z0-9 | head -c5
+        echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
-}
-install_3proxy() {
-    echo "installing 3proxy"
-    mkdir -p /3proxy
-    cd /3proxy
-    URL="https://github.com/z3APA3A/3proxy/archive/0.9.3.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.3
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
-    wget https://raw.githubusercontent.com/minhchau91/Proxy_ipv6/main/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
-    cp /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
-    systemctl link /usr/lib/systemd/system/3proxy.service
-    systemctl daemon-reload
-#    systemctl enable 3proxy
-    echo "* hard nofile 999999" >>  /etc/security/limits.conf
-    echo "* soft nofile 999999" >>  /etc/security/limits.conf
-    echo "net.ipv6.conf.${interface}.proxy_ndp=1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
-    echo "net.ipv6.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
-    sysctl -p
-    systemctl stop firewalld
-    systemctl disable firewalld
-
-    cd $WORKDIR
+        ip64() {
+                echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+        }
+        echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
 gen_3proxy() {
@@ -88,91 +60,58 @@ gen_data() {
 
 gen_iptables() {
     cat <<EOF
-    $(awk -F "|" '{print "iptables -I INPUT -p tcp --dport " $6 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+    $(awk -F "|" '{print "/sbin/iptables -I INPUT -p tcp --dport " $6 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
 EOF
 }
 
 gen_ifconfig() {
     cat <<EOF
-$(awk -F "|" '{print "ifconfig " $4 " inet6 add " $7"/"$8}' ${WORKDATA})
+$(awk -F "|" '{print "/sbin/ifconfig " $4 " inet6 add " $7"/"$8}' ${WORKDATA})
 EOF
 }
-echo "installing apps"
-yum -y install gcc net-tools bsdtar zip make >/dev/null
 
+/sbin/service network restart
+#/sbin/iptables -F INPUT
+echo "installing apps"
+rm -fv /usr/local/etc/3proxy/3proxy.cfg
+rm -fv /home/proxy-installer/data.txt
+rm -fv /home/proxy-installer/boot_iptables.sh
+rm -fv /home/proxy-installer/boot_ifconfig.sh
 echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $_
+WORKDATA2="${WORKDIR}/ipv6-subnet.txt"
 
 IP4=$(curl -4 -s icanhazip.com)
-checkIP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
-echo "Detected your ipv4: $IP4" 
-echo "Detected your ipv6: $checkIP6" 
-#read -p "What is your ipv6 prefix? (exp: /56, /64): " Prefix
-Prefix=64
-read -p "What is your ipv6 subnet? (exp: 2600:3c00:e002:6d00): " IP6
-#checkinterface=$(ip addr show | awk '/inet.*brd/{print $NF}')
-echo "Detected your active interface: $checkinterface"
-#read -p "Please confirm your active network interface : " interface
-interface=eth0
-
-#while true; do
-#    read -p "Do you want to create auth for your proxy? (Y/N): " authConfirm
-#    case $authConfirm in
-#        [Yy]* ) Auth=strong; read -p "Input UserName? " User; read -p "Input PassWord: " Pass; break;;
-#        [Nn]* ) Auth=none;User=minhchau; Pass=minhchau@123; break;;
-#        * ) echo "Please answer yes or no.";;
-#    esac
-#done
-Auth=strong
-User=mcproxy
-Pass=mcproxy2023
-
-#read -p "Please input start port :" FIRST_PORT
-#read -p "Please input start port :" LAST_PORT
+IP6=$(awk -F "|" '{print $1}' ${WORKDATA2})
+Prefix=$(awk -F "|" '{print $2}' ${WORKDATA2})
+User=$(awk -F "|" '{print $3}' ${WORKDATA2})
+Pass=$(awk -F "|" '{print $4}' ${WORKDATA2})
+interface=$(awk -F "|" '{print $5}' ${WORKDATA2})
+Auth=$(awk -F "|" '{print $6}' ${WORKDATA2})
+#FIRST_PORT=$(awk -F "|" '{print $7}' ${WORKDATA2})
+#LAST_PORT=$(awk -F "|" '{print $7}' ${WORKDATA2})
 FIRST_PORT=40000
-LAST_PORT=40349
-
-rm -fv $WORKDIR/ipv6-subnet.txt
-cat >>$WORKDIR/ipv6-subnet.txt <<EOF
-${IP6}|${Prefix}|${User}|${Pass}|${interface}|${Auth}|${IP4}|${FIRST_PORT}|${LAST_PORT}
-EOF
-
-
-install_3proxy
+LAST_PORT=40346
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-
-
 gen_data >$WORKDIR/data.txt
-gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
 chmod +x $WORKDIR/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
+rm -fv /etc/rc.local
+
 cat >>/etc/rc.local <<EOF
+touch /var/lock/subsys/local
 systemctl start NetworkManager.service
-ifup ${interface}
-bash ${WORKDIR}/boot_iptables.sh
+/sbin/ifup ${interface}
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 65535
+/bin/pkill -f '/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg'
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
 EOF
-
+chmod +x /etc/rc.local
 bash /etc/rc.local
-
-gen_proxy_file_for_user
-
-wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/Rotation.sh" --output-document=/root/Rotation.sh
-chmod 777 /root/Rotation.sh
-cat >>/var/spool/cron/root<<EOF
-#day - time
-59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
-#minutes
-#*30 * * * * /root/Rotation.sh > /root/Rotation_log.txt
-#hour
-#0 * * * * /root/Rotation.sh > /root/Rotation_log.txt
-EOF
