6cd019c6625142b607b5487f7ab2cfec5a335aa8
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-01-13 07:33:18 +0300

    Update Create_proxy.sh

--- Create_proxy.sh
+++ Create_proxy.sh
@@ -115,7 +115,7 @@ read -p "What is your ipv6 subnet? (exp: 2600:3c00:e002:6d00): " IP6
 #checkinterface=$(ip addr show | awk '/inet.*brd/{print $NF}')
 echo "Detected your active interface: $checkinterface"
 #read -p "Please confirm your active network interface : " interface
-interface=eth0
+interface=enp2s0
 
 #while true; do
 #    read -p "Do you want to create auth for your proxy? (Y/N): " authConfirm
@@ -132,11 +132,11 @@ Pass=mcproxy2023
 #read -p "Please input start port :" FIRST_PORT
 #read -p "Please input start port :" LAST_PORT
 FIRST_PORT=40000
-LAST_PORT=40349
+LAST_PORT=41099
 
 rm -fv $WORKDIR/ipv6-subnet.txt
 cat >>$WORKDIR/ipv6-subnet.txt <<EOF
-${IP6}|${Prefix}|${User}|${Pass}|${interface}|${Auth}|${IP4}|${FIRST_PORT}|${LAST_PORT}
+${IP6}|${Prefix}|${User}|${Pass}|${interface}|${Auth}
 EOF
 
 
