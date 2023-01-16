147b36f06459fc52288152b6acdcbc9e961ca8b4
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-01-16 12:43:33 +0300

    Update Rotation.sh

--- Rotation.sh
+++ Rotation.sh
@@ -94,7 +94,7 @@ Auth=$(awk -F "|" '{print $6}' ${WORKDATA2})
 #FIRST_PORT=$(awk -F "|" '{print $7}' ${WORKDATA2})
 #LAST_PORT=$(awk -F "|" '{print $7}' ${WORKDATA2})
 FIRST_PORT=30000
-LAST_PORT=30199
+LAST_PORT=30219
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
