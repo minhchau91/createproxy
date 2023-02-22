3953adb44fe7638e1c0210b1d07b9bd68427caa4
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-02-22 06:56:55 +0300

    Update Create_proxy.sh

--- Create_proxy.sh
+++ Create_proxy.sh
@@ -176,10 +176,10 @@ cat >>/var/spool/cron/root<<EOF
 #0 2 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 14 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #minutes
-#*30 * * * * /root/Rotation.sh > /root/Rotation_log.txt
+*/30 * * * * /root/Rotation.sh > /root/Rotation_log.txt
 #*/10 * * * * /root/Rotation.sh > /root/Rotation_log.txt
 #hour
-0 * * * * /root/Rotation.sh > /root/Rotation_log.txt
+#0 * * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 */4 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 */2 * * * /root/Rotation.sh > /root/Rotation_log.txt
 EOF
