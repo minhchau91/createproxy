2b712189e47d89eb7c8adc12b9dfc85cb791cc94
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-03-21 06:28:19 +0300

    Update Create_proxy.sh

--- Create_proxy.sh
+++ Create_proxy.sh
@@ -171,7 +171,7 @@ wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/Rotation.sh"
 chmod 777 /root/Rotation.sh
 cat >>/var/spool/cron/root<<EOF
 #day - time
-59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
+#59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #59 21 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 2 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 14 * * * /root/Rotation.sh > /root/Rotation_log.txt
@@ -180,6 +180,6 @@ cat >>/var/spool/cron/root<<EOF
 #*/15 * * * * /root/Rotation.sh > /root/Rotation_log.txt
 #hour
 #0 * * * * /root/Rotation.sh > /root/Rotation_log.txt
-#0 */12 * * * /root/Rotation.sh > /root/Rotation_log.txt
+0 */12 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 */2 * * * /root/Rotation.sh > /root/Rotation_log.txt
 EOF
