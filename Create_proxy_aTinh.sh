c62d8499b4622de0fef0ec831c440b2bb947b1e7
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-03-29 17:32:26 +0300

    Update Create_proxy_aTinh.sh

--- Create_proxy_aTinh.sh
+++ Create_proxy_aTinh.sh
@@ -169,9 +169,9 @@ gen_proxy_file_for_user
 
 wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/Rotation_Atinh.sh" --output-document=/root/Rotation.sh
 chmod 777 /root/Rotation.sh
-cat >>/var/spool/cron/root<<EOF
+#cat >>/var/spool/cron/root<<EOF
 #day - time
-59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
+#59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #59 21 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 2 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 14 * * * /root/Rotation.sh > /root/Rotation_log.txt
@@ -182,4 +182,4 @@ cat >>/var/spool/cron/root<<EOF
 #0 * * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 */12 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 */2 * * * /root/Rotation.sh > /root/Rotation_log.txt
-EOF
+#EOF
