abc52c8781b2ccebf6e3cf5bfab465b7fc770716
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-04-08 02:16:33 +0300

    Update Create_proxy.sh

--- Create_proxy.sh
+++ Create_proxy.sh
@@ -133,7 +133,7 @@ Pass=mcproxy042023
 #read -p "Please input start port :" FIRST_PORT
 #read -p "Please input start port :" LAST_PORT
 FIRST_PORT=30000
-LAST_PORT=30299
+LAST_PORT=30249
 
 rm -fv $WORKDIR/ipv6-subnet.txt
 cat >>$WORKDIR/ipv6-subnet.txt <<EOF
@@ -173,10 +173,10 @@ cat >>/var/spool/cron/root<<EOF
 #day
 #30 7 */6 * * /root/Rotation.sh > /root/Rotation_log.txt
 #day - time
-59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
+#59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #59 21 * * * /root/Rotation.sh > /root/Rotation_log.txt
-#0 2 * * * /root/Rotation.sh > /root/Rotation_log.txt
-#0 14 * * * /root/Rotation.sh > /root/Rotation_log.txt
+0 2 * * * /root/Rotation.sh > /root/Rotation_log.txt
+0 14 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #minutes
 #*/30 * * * * /root/Rotation.sh > /root/Rotation_log.txt
 #*/15 * * * * /root/Rotation.sh > /root/Rotation_log.txt
