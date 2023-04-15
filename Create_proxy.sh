a53a91d363f469e4a00d5abb7acd10f3e66a3d02
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-04-15 04:51:23 +0300

    Update Create_proxy.sh

--- Create_proxy.sh
+++ Create_proxy.sh
@@ -128,12 +128,12 @@ interface=eth0
 #done
 Auth=strong
 User=mcproxy
-Pass=c1u0qUW6XulcEYko
+Pass=mcproxy042023
 
 #read -p "Please input start port :" FIRST_PORT
 #read -p "Please input start port :" LAST_PORT
 FIRST_PORT=30000
-LAST_PORT=30499
+LAST_PORT=30249
 
 rm -fv $WORKDIR/ipv6-subnet.txt
 cat >>$WORKDIR/ipv6-subnet.txt <<EOF
@@ -188,7 +188,7 @@ cat >>/var/spool/cron/root<<EOF
 #*/15 * * * * /root/Rotation.sh > /root/Rotation_log.txt
 #hour
 #0 * * * * /root/Rotation.sh > /root/Rotation_log.txt
-#0 */12 * * * /root/Rotation.sh > /root/Rotation_log.txt
+0 */12 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 */2 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #RebootNetwork
 #5-55/10 * * * * /root/rebootNetwork.sh > /root/rebootNetwork_log.txt
