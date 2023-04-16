a770e692cfe60614cae5023f1bdc9d5e2611c1a1
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-04-16 06:17:26 +0300

    Update create_large_proxy.sh

--- create_large_proxy.sh
+++ create_large_proxy.sh
@@ -161,6 +161,7 @@ cat >>/etc/rc.local <<EOF
 systemctl start NetworkManager.service
 ifup ${interface}
 bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 65535
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
 EOF
