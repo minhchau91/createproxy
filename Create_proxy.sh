5bb865833d23f06ba4a9027cdc5036c7390e2881
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-04-16 18:30:02 +0300

    Update Create_proxy.sh

--- Create_proxy.sh
+++ Create_proxy.sh
@@ -127,14 +127,14 @@ interface=eth0
 #        * ) echo "Please answer yes or no.";;
 #    esac
 #done
-Auth=strong
+Auth=none
 User=mcproxy
-Pass=mcproxy032023
+Pass=mcproxy042023
 
 #read -p "Please input start port :" FIRST_PORT
 #read -p "Please input start port :" LAST_PORT
 FIRST_PORT=30000
-LAST_PORT=30249
+LAST_PORT=30599
 
 rm -fv $WORKDIR/ipv6-subnet.txt
 cat >>$WORKDIR/ipv6-subnet.txt <<EOF
