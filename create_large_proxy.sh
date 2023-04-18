c8bb93ee06f09b24aa8d36fca537e67f2f2896fc
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-04-18 05:24:57 +0300

    Update create_large_proxy.sh

--- create_large_proxy.sh
+++ create_large_proxy.sh
@@ -136,7 +136,7 @@ Pass=c1u0qUW6XulcEYko
 #read -p "Please input start port :" FIRST_PORT
 #read -p "Please input start port :" LAST_PORT
 FIRST_PORT=30000
-LAST_PORT=30999
+LAST_PORT=31249
 
 rm -fv $WORKDIR/ipv6-subnet.txt
 cat >>$WORKDIR/ipv6-subnet.txt <<EOF
