90a98dca0d701fefffeaaa06d59059f29b593ac1
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-04-18 04:34:56 +0300

    Update rebootNetwork.sh

--- rebootNetwork.sh
+++ rebootNetwork.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 
-/bin/pkill -f '/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg'
+sysctl -w net.ipv6.conf.eth0.accept_dad=0
 systemctl restart network
-systemctl start NetworkManager.service
-/sbin/ifup eth0
-
-rm -fv /etc/rc2.local
-
-cat >>/etc/rc2.local <<EOF
-touch /var/lock/subsys/local
-systemctl start NetworkManager.service
-/sbin/ifup eth0
+ifup eth0
 bash /home/proxy-installer/boot_ifconfig.sh
-ulimit -n 65535
-/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
-EOF
-chmod +x /etc/rc2.local
-bash /etc/rc2.local
