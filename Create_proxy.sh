1606a9eb603b32b4e4cff0cf1035e5b2b07e0a37
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-04-19 13:43:01 +0300

    Update Create_proxy.sh

--- Create_proxy.sh
+++ Create_proxy.sh
@@ -192,5 +192,5 @@ cat >>/var/spool/cron/root<<EOF
 #0 */12 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 */2 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #RebootNetwork
-#5-55/10 * * * * /root/rebootNetwork.sh > /root/rebootNetwork_log.txt
+30-55/30 * * * * /root/rebootNetwork.sh > /root/rebootNetwork_log.txt
 EOF
