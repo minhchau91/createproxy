f07ea477df264626ca06f8afe126bf4af63c454d
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-04-24 08:31:50 +0300

    Update rebootNetwork.sh

--- rebootNetwork.sh
+++ rebootNetwork.sh
@@ -2,5 +2,12 @@
 
 sysctl -w net.ipv6.conf.eth0.accept_dad=0
 systemctl restart network
+cat >>/etc/rc2.local <<EOF
+touch /var/lock/subsys/local
+systemctl start NetworkManager.service
 /sbin/ifup eth0
 bash /home/proxy-installer/boot_ifconfig.sh
+ulimit -n 65535
+EOF
+chmod +x /etc/rc2.local
+bash /etc/rc2.local
