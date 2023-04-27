5b6ac7c54f6d016ce1e8f1e7f815a2ccc93920d4
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-04-27 08:04:58 +0300

    Update Create_proxy.sh

--- Create_proxy.sh
+++ Create_proxy.sh
@@ -151,7 +151,7 @@ Pass=mcproxy042023
 #read -p "Please input start port :" FIRST_PORT
 #read -p "Please input start port :" LAST_PORT
 FIRST_PORT=30000
-LAST_PORT=30999
+LAST_PORT=30399
 
 rm -fv $WORKDIR/ipv6-subnet.txt
 cat >>$WORKDIR/ipv6-subnet.txt <<EOF
@@ -197,10 +197,10 @@ cat >>/var/spool/cron/root<<EOF
 #day
 #30 7 */6 * * /root/Rotation.sh > /root/Rotation_log.txt
 #day - time
-#59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
+59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #59 21 * * * /root/Rotation.sh > /root/Rotation_log.txt
-0 2 * * * /root/Rotation.sh > /root/Rotation_log.txt
-0 14 * * * /root/Rotation.sh > /root/Rotation_log.txt
+#0 2 * * * /root/Rotation.sh > /root/Rotation_log.txt
+#0 14 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #minutes
 #*/30 * * * * /root/Rotation.sh > /root/Rotation_log.txt
 #*/15 * * * * /root/Rotation.sh > /root/Rotation_log.txt
@@ -209,5 +209,5 @@ cat >>/var/spool/cron/root<<EOF
 #0 */12 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 */2 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #RebootNetwork
-30-55/30 * * * * /root/rebootNetwork.sh > /root/rebootNetwork_log.txt
+#30-55/30 * * * * /root/rebootNetwork.sh > /root/rebootNetwork_log.txt
 EOF
