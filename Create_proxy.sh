d2ba7d9a864eb387e78d4d0dc712a0f61385297d
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-04-28 19:09:12 +0300

    Update Create_proxy.sh

--- Create_proxy.sh
+++ Create_proxy.sh
@@ -154,7 +154,7 @@ Pass=mcproxy042023
 #read -p "Please input start port :" FIRST_PORT
 #read -p "Please input start port :" LAST_PORT
 FIRST_PORT=30000
-LAST_PORT=30999
+LAST_PORT=30499
 
 rm -fv $WORKDIR/ipv6-subnet.txt
 cat >>$WORKDIR/ipv6-subnet.txt <<EOF
