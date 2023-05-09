4d4078d777c1920718d1c5f6b62d848867ab8332
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-05-09 09:18:41 +0300

    Update Create_proxy.sh

--- Create_proxy.sh
+++ Create_proxy.sh
@@ -199,9 +199,9 @@ chmod 777 /root/rebootNetwork.sh
 #Add Cronjob
 cat >>/var/spool/cron/root<<EOF
 #day
-#00 7 */5 * * /root/Rotation.sh > /root/Rotation_log.txt
+00 7 */7 * * /root/Rotation.sh > /root/Rotation_log.txt
 #day - time
-59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
+#59 7 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #59 21 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 2 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 14 * * * /root/Rotation.sh > /root/Rotation_log.txt
@@ -213,5 +213,5 @@ cat >>/var/spool/cron/root<<EOF
 #0 */12 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #0 */2 * * * /root/Rotation.sh > /root/Rotation_log.txt
 #RebootNetwork
-#30-55/30 * * * * /root/rebootNetwork.sh > /root/rebootNetwork_log.txt
+30-55/30 */4 * * * /root/rebootNetwork.sh > /root/rebootNetwork_log.txt
 EOF
