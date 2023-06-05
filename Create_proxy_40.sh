9ef7434598f849aeb6e68f6af6d5715f90a2bd17
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-06-05 11:21:50 +0300

    Update Create_proxy_40.sh

--- Create_proxy_40.sh
+++ Create_proxy_40.sh
@@ -129,7 +129,7 @@ checkIP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Detected your ipv4: $IP4" 
 echo "Detected your ipv6: $checkIP6" 
 #read -p "What is your ipv6 prefix? (exp: /56, /64): " Prefix
-Prefix=48
+Prefix=40
 read -p "What is your ipv6 subnet? (exp: 2600:3c00:e002): " IP6
 #checkinterface=$(ip addr show | awk '/inet.*brd/{print $NF}')
 echo "Detected your active interface: $checkinterface"
