a0d36999dd5df15526f20956af1de1300d59aa0e
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-12-07 15:56:24 +0700

    Update edit_miniZeph.sh

--- edit_miniZeph.sh
+++ edit_miniZeph.sh
@@ -1,4 +1,6 @@
 #!/bin/sh 
 cores=$(nproc --all)
+echo "Cores: $cores" 
 rounded_cores=$((cores * 9 / 10))
+echo "rounded_cores: $rounded_cores" 
 sed -i 's/--cpu-max-threads-hint=90/--threads=$rounded_cores/g' danielchau.sh
