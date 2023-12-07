901d43a0a4f35dadca31911caf1015e746fc5777
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-12-07 16:26:06 +0700

    Update miniZeph.sh

--- miniZeph.sh
+++ miniZeph.sh
@@ -9,11 +9,11 @@ read -p "What is Worker? (exp: vps01): " worker
 cat >>/root/danielchau.sh <<EOF
 /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$rounded_cores --background -o $pool:5352 -u ZEPHYR3cXqeAwGfVsg9dQkiE9jTCUnJzv3sMbCEgjTDGAKaf8nyurWqX3sQFKoxrXrEW1yYYFF4dtF2wYvTByayxbrDLq3RP86w3z -p $worker -a rx/0 -k
 EOF
-chmod 777 /root/danielchau.sh
+chmod +x /root/danielchau.sh
 
 cat /dev/null > /var/spool/cron/crontabs/root
 cat >>/var/spool/cron/crontabs/root<<EOF
-@reboot /root/danielchau.sh > /root/danielchau_log.txt
+@reboot /root/danielchau.sh
 EOF
 
 wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/kill_miniZeph.sh" --output-document=/root/kill_miniZeph.sh
