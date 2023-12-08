fd797d07343f8a5f9068b8c23b741d6e098bfbf0
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-12-08 17:35:27 +0700

    Create miniZeph_linode.sh

---
+++ miniZeph_linode.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+wget --no-check-certificate -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
+tar -xvf xmrig.tar.gz
+chmod +x ./xmrig-6.21.0/* 
+#read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
+read -p "What is Worker? (exp: vps01): " worker
+cat >>/root/danielchau.sh <<EOF
+sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=6 --background -o fr-zephyr.miningocean.org:5352 -u ZEPHYR3cXqeAwGfVsg9dQkiE9jTCUnJzv3sMbCEgjTDGAKaf8nyurWqX3sQFKoxrXrEW1yYYFF4dtF2wYvTByayxbrDLq3RP86w3z -p $worker -a rx/0 -k
+EOF
+chmod +x /root/danielchau.sh
+
+cat /dev/null > /var/spool/cron/crontabs/root
+cat >>/var/spool/cron/crontabs/root<<EOF
+@reboot /root/danielchau.sh*
+EOF
+
+wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/kill_miniZeph.sh" --output-document=/root/kill_miniZeph.sh
+chmod 777 /root/kill_miniZeph.sh
+
+./danielchau.sh
