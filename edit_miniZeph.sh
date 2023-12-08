b845eb9d63516a6147376b19abfeced23651890c
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-12-08 19:55:46 +0700

    Update edit_miniZeph.sh

--- edit_miniZeph.sh
+++ edit_miniZeph.sh
@@ -3,4 +3,7 @@ cores=$(nproc --all)
 echo "Cores: $cores" 
 rounded_cores=$((cores * 9 / 10))
 echo "rounded_cores: $rounded_cores" 
-sed -i 's/--cpu-max-threads-hint=90/--threads=${rounded_cores}/g' danielchau.sh
+limitCPU=$((cores * 90))
+echo "limitCPU: $limitCPU" 
+sed -i 's/--cpu-max-threads-hint=${rounded_cores}/--threads=${cores}/g' danielchau.sh
+sed -i -e 'cpulimit --limit=$limitCPU --pid $(pgrep xmrig)  > /dev/null 2>&1 &' file
