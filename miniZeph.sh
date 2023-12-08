1083fc5779d0443f4ced18592dc9318b5c1bde03
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-12-08 19:44:39 +0700

    Update miniZeph.sh

--- miniZeph.sh
+++ miniZeph.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
+sudo apt-get update -y
+sudo apt-get install cpulimit -y
 wget --no-check-certificate -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
 tar -xvf xmrig.tar.gz
 chmod +x ./xmrig-6.21.0/* 
 cores=$(nproc --all)
-rounded_cores=$((cores * 9 / 10))
+#rounded_cores=$((cores * 9 / 10))
 #read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
 read -p "What is Worker? (exp: vps01): " worker
+limitCPU=$((cores * 90))
 cat >>/root/danielchau.sh <<EOF
-sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$rounded_cores --background -o ca-zephyr.miningocean.org:5352 -u ZEPHYR3cXqeAwGfVsg9dQkiE9jTCUnJzv3sMbCEgjTDGAKaf8nyurWqX3sQFKoxrXrEW1yYYFF4dtF2wYvTByayxbrDLq3RP86w3z -p $worker -a rx/0 -k
+sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$cores --background -o ca-zephyr.miningocean.org:5352 -u ZEPHYR3cXqeAwGfVsg9dQkiE9jTCUnJzv3sMbCEgjTDGAKaf8nyurWqX3sQFKoxrXrEW1yYYFF4dtF2wYvTByayxbrDLq3RP86w3z -p $worker -a rx/0 -k
+cpulimit --limit=$limitCPU --pid $(pgrep xmrig) > /dev/null 2>&1 &
 EOF
 chmod +x /root/danielchau.sh
 
@@ -20,3 +24,4 @@ wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/kill_miniZep
 chmod 777 /root/kill_miniZeph.sh
 
 ./danielchau.sh
+
