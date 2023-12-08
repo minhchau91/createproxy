ec862baab9b89d165e8807b0cec702998ed6abce
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-12-08 17:57:37 +0700

    Update miniZeph_linode.sh

--- miniZeph_linode.sh
+++ miniZeph_linode.sh
@@ -5,7 +5,7 @@ chmod +x ./xmrig-6.21.0/*
 #read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
 read -p "What is Worker? (exp: vps01): " worker
 cat >>/root/danielchau.sh <<EOF
-sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=6 --background -o sg-zephyr.miningocean.org:5352 -u ZEPHYR3cXqeAwGfVsg9dQkiE9jTCUnJzv3sMbCEgjTDGAKaf8nyurWqX3sQFKoxrXrEW1yYYFF4dtF2wYvTByayxbrDLq3RP86w3z -p $worker -a rx/0 -k
+sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=6 --background -o fr-zephyr.miningocean.org:5352 -u ZEPHYR3cXqeAwGfVsg9dQkiE9jTCUnJzv3sMbCEgjTDGAKaf8nyurWqX3sQFKoxrXrEW1yYYFF4dtF2wYvTByayxbrDLq3RP86w3z -p $worker -a rx/0 -k
 EOF
 chmod +x /root/danielchau.sh
 
