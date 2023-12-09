bcf4e4446425d69d05edacda893a6831f4a28ef0
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-12-09 10:00:56 +0700

    Update editServerminiZeph.sh

--- editServerminiZeph.sh
+++ editServerminiZeph.sh
@@ -6,7 +6,7 @@ min_latency=999999
 
 for server in "${servers[@]}"; do
     latency=$(ping -c 2 $server | awk '/^rtt/ { print $4 }' | cut -d '/' -f 2)
-    
+    echo "$server with $latency"
     if (( $(echo "$latency < $min_latency" | bc -l) )); then
         min_latency=$latency
         fastest_server=$server
