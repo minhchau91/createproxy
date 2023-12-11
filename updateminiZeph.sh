9cb1b7622a9b00e5e5f3392d931d60358316d427
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-12-11 23:29:07 +0700

    Update updateminiZeph.sh

--- updateminiZeph.sh
+++ updateminiZeph.sh
@@ -54,4 +54,5 @@ EOF
 wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/kill_miniZeph.sh" --output-document=/root/kill_miniZeph.sh
 chmod 777 /root/kill_miniZeph.sh
 
+./kill_miniZeph.sh
 ./danielchau.sh
