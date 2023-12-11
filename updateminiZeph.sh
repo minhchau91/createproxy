87c1499f10c325456d6156ab2dc480339deb8214
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-12-11 09:12:05 +0700

    Create updateminiZeph.sh

---
+++ updateminiZeph.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cat /dev/null > /root/checkXMRIG.sh
+cat >>/root/checkXMRIG.sh <<EOF
+#!/bin/bash
+if pgrep xmrig >/dev/null
+then
+  echo "xmrig is running."
+else
+  echo "xmrig isn't running"
+  bash kill_miniZeph.sh
+  bash danielchau.sh
+fi
+EOF
+chmod +x /root/checkXMRIG.sh
+
+cat /dev/null > /var/spool/cron/crontabs/root
+cat >>/var/spool/cron/crontabs/root<<EOF
+@reboot /root/checkXMRIG.sh > /root/checkxmrig.log
+*/10 * * * * /root/checkXMRIG.sh > /root/checkxmrig.log
+EOF
