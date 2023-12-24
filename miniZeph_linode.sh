beff69334ed5c67ad65936392848384c9a6242af
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-12-24 11:49:19 +0700

    Update miniZeph_linode.sh

--- miniZeph_linode.sh
+++ miniZeph_linode.sh
@@ -20,7 +20,7 @@ echo "$fastest_server with min_latency is: $latency"
 cat /dev/null > /root/danielchau.sh
 cat >>/root/danielchau.sh <<EOF
 #!/bin/bash
-sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=6 --background -o $fastest_server:5352 -u ZEPHYR3cXqeAwGfVsg9dQkiE9jTCUnJzv3sMbCEgjTDGAKaf8nyurWqX3sQFKoxrXrEW1yYYFF4dtF2wYvTByayxbrDLq3RP86w3z -p Linode_waltertheran476987 -a rx/0 -k
+sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=6 --background -o $fastest_server:5352 -u ZEPHYR3cXqeAwGfVsg9dQkiE9jTCUnJzv3sMbCEgjTDGAKaf8nyurWqX3sQFKoxrXrEW1yYYFF4dtF2wYvTByayxbrDLq3RP86w3z -p Linode_hadenphilo575 -a rx/0 -k
 EOF
 chmod +x /root/danielchau.sh
 
