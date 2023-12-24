0f22ffece7944a15ba21139043c2a3a551b2493d
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-12-24 16:17:41 +0700

    Update miniNicehash.sh

--- miniNicehash.sh
+++ miniNicehash.sh
@@ -1,30 +1,16 @@
 #!/bin/sh
 ./kill_miniZeph.sh
-mv /root/danielchau.sh /root/mininZeph.sh
+cp -f danielchau.sh mininZeph.sh 
+extracted_string=$(sed -n 's/.*-p \([^ ]*\).*/\1/p' /root/danielchau.sh)
+cores=$(nproc --all)
+limitCPU=$((cores * 80))
+cat /dev/null > /root/danielchau.sh
 cat >>/root/danielchau.sh <<EOF
 #!/bin/bash
-sudo /root/xmrig-6.21.0/xmrig --threads=6 --background -o randomxmonero.auto.nicehash.com:9200 -u NHbVF7wPddHyFthiCiA4yuc6YU916LHbgSJB.Linode_H1 -a rx/0 -k
+sudo /root/xmrig-6.21.0/xmrig --threads=$cores --background -o randomxmonero.auto.nicehash.com:9200 -u NHbVF7wPddHyFthiCiA4yuc6YU916LHbgSJB.$extracted_string -a rx/0 -k
 EOF
 chmod +x /root/danielchau.sh
 
-cat /dev/null > /root/checkXMRIG.sh
-cat >>/root/checkXMRIG.sh <<EOF
-#!/bin/bash
-if pgrep xmrig >/dev/null
-then
-  echo "xmrig is running."
-else
-  echo "xmrig isn't running"
-  bash kill_miniZeph.sh
-  bash danielchau.sh
-fi
-EOF
-chmod +x /root/checkXMRIG.sh
-
-cat /dev/null > /var/spool/cron/crontabs/root
-cat >>/var/spool/cron/crontabs/root<<EOF
-@reboot /root/danielchau.sh
-*/10 * * * * /root/checkXMRIG.sh > /root/checkxmrig.log
-EOF
+sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" danielchau.sh
 
 ./danielchau.sh
