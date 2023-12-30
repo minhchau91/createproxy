951c4fd03bdcbb8d5c66e5149849f3a786c96efd
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2023-12-30 18:46:48 +0700

    Update miniNicehash.sh

--- miniNicehash.sh
+++ miniNicehash.sh
@@ -13,9 +13,11 @@ limitCPU=$((cores * 80))
 cat /dev/null > /root/danielchau.sh
 cat >>/root/danielchau.sh <<EOF
 #!/bin/bash
-sudo /root/xmrig-6.21.0/xmrig --threads=$cores --background -o randomxmonero.auto.nicehash.com:9200 -u NHbVF7wPddHyFthiCiA4yuc6YU916LHbgSJB.camelliatoste@outlook.com -a rx/0 -k
+sudo /root/xmrig-6.21.0/xmrig --threads=$cores --background -o randomxmonero.auto.nicehash.com:9200 -u NHbVF7wPddHyFthiCiA4yuc6YU916LHbgSJB.vultr30-12 -a rx/0 -k
 EOF
 chmod +x /root/danielchau.sh
+
+sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" danielchau.sh
 
 cat /dev/null > /root/checkXMRIG.sh
 cat >>/root/checkXMRIG.sh <<EOF
