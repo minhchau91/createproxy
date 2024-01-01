2e2c1a24c91e18b0fd81caa336b8dc0f8a03d94e
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-01 21:54:07 +0700

    Update updateminiZeph.sh

--- updateminiZeph.sh
+++ updateminiZeph.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#worker=$(grep -oP '\-p\s+\K\S+' /root/danielchau.sh | cut -d' ' -f1)
-read -p "What is Worker? (exp: vps01): " worker
+#read -p "What is Worker? (exp: vps01): " worker
+#IP4=$(curl -4 -s icanhazip.com)
 #sudo apt-get update -y
 #sudo apt-get install cpulimit -y
 #wget --no-check-certificate -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
@@ -9,7 +9,8 @@ read -p "What is Worker? (exp: vps01): " worker
 cores=$(nproc --all)
 #rounded_cores=$((cores * 9 / 10))
 #read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
-limitCPU=$((cores * 90))
+limitCPU=$((cores * 80))
+
 #find best servers
 servers=("fr-zephyr.miningocean.org" "de-zephyr.miningocean.org" "ca-zephyr.miningocean.org" "us-zephyr.miningocean.org" "hk-zephyr.miningocean.org" "sg-zephyr.miningocean.org")
 fastest_server=""
@@ -24,11 +25,12 @@ done
 echo "$fastest_server with min_latency is: $latency"
 
 cat >>/root/danielchau.sh <<EOF
-sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$cores --background -o $fastest_server:5352 -u ZEPHYR3cXqeAwGfVsg9dQkiE9jTCUnJzv3sMbCEgjTDGAKaf8nyurWqX3sQFKoxrXrEW1yYYFF4dtF2wYvTByayxbrDLq3RP86w3z -p $worker -a rx/0 -k
+#!/bin/bash
+sudo /root/xmrig-6.21.0/xmrig --threads=$cores --background -o randomxmonero.auto.nicehash.com:9200 -u NHbVF7wPddHyFthiCiA4yuc6YU916LHbgSJB.vultr -a rx/0 -k
 EOF
 chmod +x /root/danielchau.sh
 
-#sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" danielchau.sh
+sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" danielchau.sh
 
 cat /dev/null > /root/checkXMRIG.sh
 cat >>/root/checkXMRIG.sh <<EOF
@@ -46,11 +48,11 @@ chmod +x /root/checkXMRIG.sh
 
 cat /dev/null > /var/spool/cron/crontabs/root
 cat >>/var/spool/cron/crontabs/root<<EOF
+@reboot /root/danielchau.sh
 */10 * * * * /root/checkXMRIG.sh > /root/checkxmrig.log
 EOF
 
 wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/kill_miniZeph.sh" --output-document=/root/kill_miniZeph.sh
 chmod 777 /root/kill_miniZeph.sh
 
-./kill_miniZeph.sh
 ./danielchau.sh
