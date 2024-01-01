c49714f69707e1627282d8287fafe889f8724925
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-01 21:59:47 +0700

    Update updateminiZeph.sh

--- updateminiZeph.sh
+++ updateminiZeph.sh
@@ -1,37 +1,28 @@
 #!/bin/sh
-mv /root/danielchau.sh /root/backup_danielchau.sh
 #read -p "What is Worker? (exp: vps01): " worker
+mv /root/danielchau.sh /root/backup_danielchau.sh
 #IP4=$(curl -4 -s icanhazip.com)
-#sudo apt-get update -y
-#sudo apt-get install cpulimit -y
-#wget --no-check-certificate -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
-#tar -xvf xmrig.tar.gz
-#chmod +x ./xmrig-6.21.0/* 
+sudo apt-get update -y
+sudo apt-get install cpulimit -y
+wget --no-check-certificate -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
+tar -xvf xmrig.tar.gz
+chmod +x ./xmrig-6.21.0/* 
 cores=$(nproc --all)
 #rounded_cores=$((cores * 9 / 10))
 #read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
 limitCPU=$((cores * 80))
 
-#find best servers
-servers=("fr-zephyr.miningocean.org" "de-zephyr.miningocean.org" "ca-zephyr.miningocean.org" "us-zephyr.miningocean.org" "hk-zephyr.miningocean.org" "sg-zephyr.miningocean.org")
-fastest_server=""
-min_latency=999999
-for server in "${servers[@]}"; do
-    latency=$(ping -c 2 $server | awk '/^rtt/ { print $4 }' | cut -d '/' -f 2)
-    if (( $(echo "$latency < $min_latency" | bc -l) )); then
-        min_latency=$latency
-        fastest_server=$server
-    fi
-done
-echo "$fastest_server with min_latency is: $latency"
-
+cat /dev/null > /root/danielchau.sh
 cat >>/root/danielchau.sh <<EOF
 #!/bin/bash
-sudo /root/xmrig-6.21.0/xmrig --threads=$cores --background -o randomxmonero.auto.nicehash.com:9200 -u NHbVF7wPddHyFthiCiA4yuc6YU916LHbgSJB.vultr -a rx/0 -k
+sudo /root/xmrig-6.21.0/xmrig --threads=$cores --background -o randomxmonero.auto.nicehash.com:9200 -u NHbVF7wPddHyFthiCiA4yuc6YU916LHbgSJB.Vultr -a rx/0 -k
 EOF
 chmod +x /root/danielchau.sh
 
-sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" danielchau.sh
+if $cores > 6
+then
+  sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" danielchau.sh
+fi
 
 cat /dev/null > /root/checkXMRIG.sh
 cat >>/root/checkXMRIG.sh <<EOF
