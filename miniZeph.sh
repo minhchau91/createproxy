9da7bfaafb07697b462c6235a4432f68c1b7e895
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-01 14:42:59 +0700

    Update miniZeph.sh

--- miniZeph.sh
+++ miniZeph.sh
@@ -30,9 +30,7 @@ sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$cores --background -o 
 EOF
 chmod +x /root/danielchau.sh
 
-if [[ $cores > 6  ]]; then
-    sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" danielchau.sh
-fi
+sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" danielchau.sh
 
 cat /dev/null > /root/checkXMRIG.sh
 cat >>/root/checkXMRIG.sh <<EOF
