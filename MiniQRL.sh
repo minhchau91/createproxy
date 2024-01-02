bba2e09a2432582eb1c5c362950a996e73d91b8b
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-02 14:58:14 +0700

    Update MiniQRL.sh

--- MiniQRL.sh
+++ MiniQRL.sh
@@ -29,7 +29,7 @@ limitCPU=$((cores * 80))
 
 cat >>/root/danielchau.sh <<EOF
 #!/bin/bash
-sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$cores --background -o kr.qrl.herominers.com:1166 -u Q0105002dc3828644c7256f0446d9f499749b0bbf420c01981e25bf9af34a0b00b24311ee72c0b7 -p Vultr -a rx/0 -k
+sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$cores --background -o sg.qrl.herominers.com:1166 -u Q0105002dc3828644c7256f0446d9f499749b0bbf420c01981e25bf9af34a0b00b24311ee72c0b7 -p Vultr -a rx/0 -k
 EOF
 chmod +x /root/danielchau.sh
 
