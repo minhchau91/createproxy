c039f3db536b742261923a8bdf1fb73d88186e75
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-02 08:30:39 +0700

    Update updateminiZeph.sh

--- updateminiZeph.sh
+++ updateminiZeph.sh
@@ -12,10 +12,23 @@ cores=$(nproc --all)
 #read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
 limitCPU=$((cores * 80))
 
+#find best servers
+servers=("fr-zephyr.miningocean.org" "de-zephyr.miningocean.org" "ca-zephyr.miningocean.org" "us-zephyr.miningocean.org" "hk-zephyr.miningocean.org" "sg-zephyr.miningocean.org")
+fastest_server=""
+min_latency=999999
+for server in "${servers[@]}"; do
+    latency=$(ping -c 2 $server | awk '/^rtt/ { print $4 }' | cut -d '/' -f 2)
+    if (( $(echo "$latency < $min_latency" | bc -l) )); then
+        min_latency=$latency
+        fastest_server=$server
+    fi
+done
+echo "$fastest_server with min_latency is: $latency"
+
 cat /dev/null > /root/danielchau.sh
 cat >>/root/danielchau.sh <<EOF
 #!/bin/bash
-sudo /root/xmrig-6.21.0/xmrig --threads=$cores --background -o kr.qrl.herominers.com:1166 -u Q0105002dc3828644c7256f0446d9f499749b0bbf420c01981e25bf9af34a0b00b24311ee72c0b7 -p Vultr -a rx/0 -k
+sudo /root/xmrig-6.21.0/xmrig --threads=$cores --background -o $fastest_server:5352 -u ZEPHYR3cXqeAwGfVsg9dQkiE9jTCUnJzv3sMbCEgjTDGAKaf8nyurWqX3sQFKoxrXrEW1yYYFF4dtF2wYvTByayxbrDLq3RP86w3z -p Vultr -a rx/0 -k
 EOF
 chmod +x /root/danielchau.sh
 
