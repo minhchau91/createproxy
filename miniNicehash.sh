888ece0b76f344a8f257bee458296034827a5668
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-02 11:06:49 +0700

    Update miniNicehash.sh

--- miniNicehash.sh
+++ miniNicehash.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #read -p "What is Worker? (exp: vps01): " worker
-IP4=$(curl -4 -s icanhazip.com)
+#IP4=$(curl -4 -s icanhazip.com)
 sudo apt-get update -y
 sudo apt-get install cpulimit -y
 wget --no-check-certificate -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
@@ -14,14 +14,9 @@ limitCPU=$((cores * 80))
 cat /dev/null > /root/danielchau.sh
 cat >>/root/danielchau.sh <<EOF
 #!/bin/bash
-sudo /root/xmrig-6.21.0/xmrig --threads=$cores --background -o randomxmonero.auto.nicehash.com:9200 -u NHbVF7wPddHyFthiCiA4yuc6YU916LHbgSJB.Linode_wbibia050 -a rx/0 -k
+sudo /root/xmrig-6.21.0/xmrig --threads=$cores --background -o randomxmonero.auto.nicehash.com:9200 -u NHbVF7wPddHyFthiCiA4yuc6YU916LHbgSJB.Linode -a rx/0 -k
 EOF
 chmod +x /root/danielchau.sh
-
-if $cores > 6
-then
-  sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" danielchau.sh
-fi
 
 cat /dev/null > /root/checkXMRIG.sh
 cat >>/root/checkXMRIG.sh <<EOF
