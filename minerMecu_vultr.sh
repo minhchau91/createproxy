60384c9be3b09b142f665589b9d0145298778440
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-07 14:50:47 +0700

    Update minerMecu_vultr.sh

--- minerMecu_vultr.sh
+++ minerMecu_vultr.sh
@@ -25,7 +25,7 @@ for server in "${servers[@]}"; do
 done
 echo "$fastest_server with min_latency is: $latency"
 
-
+cat /dev/null > /root/danielchau.sh
 cat >>/root/danielchau.sh <<EOF
 #!/bin/bash
 sudo /root/xmrig-6.21.0/xmrig --background --threads=$cores -a ghostrider --url $fastest_server:17094 --tls --user Ram7FgfDBNRgK4KcUgcNfMA8c1FgFBWE5P.Vultr
