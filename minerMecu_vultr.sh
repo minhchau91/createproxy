a6b769cea11b621f804cb8bfede15ea11d6fb613
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-07 14:47:58 +0700

    Create minerMecu_vultr.sh

---
+++ minerMecu_vultr.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+#read -p "What is Worker? (exp: vps01): " worker
+#IP4=$(curl -4 -s icanhazip.com)
+rm -fv danielchau.sh
+sudo apt-get update -y
+sudo apt-get install cpulimit -y
+wget --no-check-certificate -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
+tar -xvf xmrig.tar.gz
+chmod +x ./xmrig-6.21.0/* 
+cores=$(nproc --all)
+#rounded_cores=$((cores * 9 / 10))
+#read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
+limitCPU=$((cores * 80))
+
+#find best servers
+servers=("stratum-eu.rplant.xyz" "stratum-asia.rplant.xyz" "stratum-na.rplant.xyz")
+fastest_server=""
+min_latency=999999
+for server in "${servers[@]}"; do
+    latency=$(ping -c 2 $server | awk '/^rtt/ { print $4 }' | cut -d '/' -f 2)
+    if (( $(echo "$latency < $min_latency" | bc -l) )); then
+        min_latency=$latency
+        fastest_server=$server
+    fi
+done
+echo "$fastest_server with min_latency is: $latency"
+
+
+cat >>/root/danielchau.sh <<EOF
+#!/bin/bash
+sudo /root/xmrig-6.21.0/xmrig --background --threads=$cores -a ghostrider --url $fastest_server:17094 --tls --user Ram7FgfDBNRgK4KcUgcNfMA8c1FgFBWE5P.Vultr
+EOF
+chmod +x /root/danielchau.sh
+
+sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" danielchau.sh
+
+cat /dev/null > /root/checkXMRIG.sh
+cat >>/root/checkXMRIG.sh <<EOF
+#!/bin/bash
+if pgrep xmrig >/dev/null
+then
+  echo "xmrig is running."
+else
+  echo "xmrig isn't running"
+  bash kill_miniZeph.sh
+  bash danielchau.sh
+fi
+EOF
+chmod +x /root/checkXMRIG.sh
+
+cat /dev/null > /var/spool/cron/crontabs/root
+cat >>/var/spool/cron/crontabs/root<<EOF
+*/10 * * * * /root/checkXMRIG.sh > /root/checkxmrig.log
+EOF
+
+wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/kill_miniZeph.sh" --output-document=/root/kill_miniZeph.sh
+chmod 777 /root/kill_miniZeph.sh
+
+./danielchau.sh
