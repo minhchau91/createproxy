8cf96cb1e98bc5d86fb7a993df84d952463c216e
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-07 09:03:22 +0700

    Create miniNicehash_vultr.sh

---
+++ miniNicehash_vultr.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#read -p "What is Worker? (exp: vps01): " worker
+#IP4=$(curl -4 -s icanhazip.com)
+#sudo apt-get update -y
+#sudo apt-get install cpulimit -y
+#wget --no-check-certificate -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
+#tar -xvf xmrig.tar.gz
+#chmod +x ./xmrig-6.21.0/* 
+cores=$(nproc --all)
+#rounded_cores=$((cores * 9 / 10))
+#read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
+limitCPU=$((cores * 80))
+
+cat /dev/null > /root/danielchau.sh
+cat >>/root/danielchau.sh <<EOF
+#!/bin/bash
+sudo /root/xmrig-6.21.0/xmrig --threads=$cores --background -o randomxmonero.auto.nicehash.com:9200 -u NHbVF7wPddHyFthiCiA4yuc6YU916LHbgSJB.Linode -a rx/0 -k
+EOF
+chmod +x /root/danielchau.sh
+
+sed -i "$ a\\cpulimit --limit=$limitCPU --pid \$(pidof xmrig) > /dev/null 2>&1 &" danielchau.sh
+
+cat /dev/null > /root/checkXMRIG.sh
+cat >>/root/checkXMRIG.sh <<EOF
+#!/bin/bash
+if pgrep xmrig >/dev/null
+then
+  echo "xmrig is running."
+else
+  echo "xmrig isn't running"
+  bash kill_miniZeph.sh
+  bash danielchau.sh
+fi
+EOF
+chmod +x /root/checkXMRIG.sh
+
+cat /dev/null > /var/spool/cron/crontabs/root
+cat >>/var/spool/cron/crontabs/root<<EOF
+*/5 * * * * /root/checkXMRIG.sh > /root/checkxmrig.log
+EOF
+
+wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/kill_miniZeph.sh" --output-document=/root/kill_miniZeph.sh
+chmod 777 /root/kill_miniZeph.sh
+
+./danielchau.sh
