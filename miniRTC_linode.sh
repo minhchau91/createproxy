c83c43ccf066c67a1d9251cbb0ff53619035b63f
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-10 04:12:20 +0700

    Update miniRTC_linode.sh

--- miniRTC_linode.sh
+++ miniRTC_linode.sh
@@ -8,7 +8,7 @@ chmod +x ./xmrig-6.21.0/*
 cores=$(nproc --all)
 #rounded_cores=$((cores * 9 / 10))
 #read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
-limitCPU=$((cores * 90))
+limitCPU=$((cores * 80))
 
 #find best servers
 servers=("stratum-eu.rplant.xyz" "stratum-asia.rplant.xyz" "stratum-na.rplant.xyz")
