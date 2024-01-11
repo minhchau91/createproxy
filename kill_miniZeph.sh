dca6044107506a1868003ffa09f53c16ea952ccc
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-11 13:48:43 +0700

    Update kill_miniZeph.sh

--- kill_miniZeph.sh
+++ kill_miniZeph.sh
@@ -5,3 +5,6 @@ pid2=$(pidof cpuminer-sse2)
 sudo /bin/kill $pid2
 pid3=$(pidof gpupool_miner_worker_2023_09_13_04_ubuntu16.04)
 sudo /bin/kill $pid3
+pid4=$(pidof hacash_miner_pool_worker_2022_09_09_01)
+sudo /bin/kill $pid4
+
