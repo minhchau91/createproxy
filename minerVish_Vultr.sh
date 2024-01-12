78cc7405ab03078de992e9c9a8082018f9920f2a
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-12 22:01:18 +0700

    Update minerVish_Vultr.sh

--- minerVish_Vultr.sh
+++ minerVish_Vultr.sh
@@ -34,6 +34,7 @@ cat /dev/null > /root/danielchau.sh
 cat >>/root/danielchau.sh <<EOF
 #!/bin/bash
 sudo /root/cpuminer-opt-linux/cpuminer-sse2 --background --threads=$cores -a yespower -o stratum+tcps://$fastest_server:17079 -u v3K4mds92oWPHSPuQ4Tm6bSSNMCmNj1JyY.Vultr
+sleep 2
 EOF
 chmod +x /root/danielchau.sh
 
