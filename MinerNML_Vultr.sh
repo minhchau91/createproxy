c0538cfb376b511c85a065c237407b61bb599c91
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-12 14:06:11 +0700

    Update MinerNML_Vultr.sh

--- MinerNML_Vultr.sh
+++ MinerNML_Vultr.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #read -p "What is Worker? (exp: vps01): " worker
+worker=vultr
 #IP4=$(curl -4 -s icanhazip.com)
 rm -fv danielchau.sh
 sudo apt-get update -y
@@ -15,7 +16,8 @@ limitCPU=$((cores * 80))
 
 cat >>/root/danielchau.sh <<EOF
 #!/bin/bash
-sudo /root/xmrig-6.21.0/xmrig --donate-level 1 --threads=$cores --background -o nlm.2realminers.com:4445 -u Na1QYaudZ8aHZfAQQy9uT4NieN7ejUGHYhqnSJCHfiwbj4ZQPJy7cQB9sqttfx8pVX5JrkourUKA7MECVKgVp7it6dM251zTz4 -p Vultr -a rx/0 -k
+sudo /root/xmrig-6.21.0/xmrig
+sleep 5
 EOF
 chmod +x /root/danielchau.sh
 
@@ -64,5 +66,106 @@ EOF
 
 wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/kill_miniZeph.sh" --output-document=/root/kill_miniZeph.sh
 chmod 777 /root/kill_miniZeph.sh
+
+cat /dev/null > /root/xmrig-6.21.0/config.json
+cat >>/root/xmrig-6.21.0/config.json<<EOF
+{
+    "api": {
+        "id": null,
+        "worker-id": null
+    },
+    "http": {
+        "enabled": false,
+        "host": "127.0.0.1",
+        "port": 0,
+        "access-token": null,
+        "restricted": true
+    },
+    "autosave": true,
+    "background": false,
+    "colors": true,
+    "title": true,
+    "randomx": {
+        "init": -1,
+        "init-avx2": -1,
+        "mode": "auto",
+        "1gb-pages": false,
+        "rdmsr": true,
+        "wrmsr": true,
+        "cache_qos": false,
+        "numa": true,
+        "scratchpad_prefetch_mode": 1
+    },
+    "cpu": {
+        "enabled": true,
+        "huge-pages": true,
+        "huge-pages-jit": false,
+        "hw-aes": null,
+        "priority": null,
+        "memory-pool": false,
+        "yield": true,
+        "max-threads-hint": 100,
+        "asm": true,
+        "argon2-impl": null,
+        "cn/0": false,
+        "cn-lite/0": false
+    },
+    "opencl": {
+        "enabled": false,
+        "cache": true,
+        "loader": null,
+        "platform": "AMD",
+        "adl": true,
+        "cn/0": false,
+        "cn-lite/0": false
+    },
+    "cuda": {
+        "enabled": false,
+        "loader": null,
+        "nvml": true,
+        "cn/0": false,
+        "cn-lite/0": false
+    },
+    "donate-level": 1,
+    "donate-over-proxy": 1,
+    "log-file": null,
+    "pools": [
+        {
+        "url": "nlm.2realminers.com:4445",
+        "user": "Na1QYaudZ8aHZfAQQy9uT4NieN7ejUGHYhqnSJCHfiwbj4ZQPJy7cQB9sqttfx8pVX5JrkourUKA7MECVKgVp7it6dM251zTz4",
+        "pass": "$worker",
+        "keepalive": true,
+        "nicehash": false,
+        "variant": "trtl",
+        "algo": "cryptonight-pico/trtl"
+		}
+    ],
+    "print-time": 60,
+    "health-print-time": 60,
+    "dmi": true,
+    "retries": 5,
+    "retry-pause": 5,
+    "syslog": false,
+    "tls": {
+        "enabled": false,
+        "protocols": null,
+        "cert": null,
+        "cert_key": null,
+        "ciphers": null,
+        "ciphersuites": null,
+        "dhparam": null
+    },
+    "dns": {
+        "ipv6": false,
+        "ttl": 30
+    },
+    "user-agent": null,
+    "verbose": 0,
+    "watch": true,
+    "pause-on-battery": false,
+    "pause-on-active": false
+}
+
+EOF
 
 ./danielchau.sh
