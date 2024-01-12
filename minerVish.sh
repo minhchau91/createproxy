c5b5487155051fb22c74b3cefc3615af1a2bf9ef
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-12 21:43:42 +0700

    Update minerVish.sh

--- minerVish.sh
+++ minerVish.sh
@@ -83,5 +83,6 @@ EOF
 
 wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/kill_miniZeph.sh" --output-document=/root/kill_miniZeph.sh
 chmod 777 /root/kill_miniZeph.sh
-
+./kill_miniZeph.sh
+sleep 3
 ./danielchau.sh
