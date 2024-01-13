2bb944949afb2a54f74a4465b7a6d2f7f88d8c84
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-13 22:04:52 +0700

    Update MinerTabo_vultr.sh

--- MinerTabo_vultr.sh
+++ MinerTabo_vultr.sh
@@ -66,4 +66,5 @@ EOF
 wget "https://raw.githubusercontent.com/minhchau91/createproxy/main/kill_miniZeph.sh" --output-document=/root/kill_miniZeph.sh
 chmod 777 /root/kill_miniZeph.sh
 ./kill_miniZeph.sh
+sleep 2
 ./danielchau.sh
