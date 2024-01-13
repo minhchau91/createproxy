bee31671b01a451174ef53ecc8cc4c293bef968e
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-13 21:59:51 +0700

    Update MinerTabo_vultr.sh

--- MinerTabo_vultr.sh
+++ MinerTabo_vultr.sh
@@ -10,23 +10,10 @@ cores=$(nproc --all)
 #read -p "What is pool? (exp: fr-zephyr.miningocean.org): " pool
 limitCPU=$((cores * 80))
 
-#find best servers
-servers=("stratum-eu.rplant.xyz" "stratum-asia.rplant.xyz" "stratum-na.rplant.xyz")
-fastest_server=""
-min_latency=999999
-for server in "${servers[@]}"; do
-    latency=$(ping -c 2 $server | awk '/^rtt/ { print $4 }' | cut -d '/' -f 2)
-    if (( $(echo "$latency < $min_latency" | bc -l) )); then
-        min_latency=$latency
-        fastest_server=$server
-    fi
-done
-echo "$fastest_server with min_latency is: $latency"
-
 cat /dev/null > /root/danielchau.sh
 cat >>/root/danielchau.sh <<EOF
 #!/bin/bash
-sudo /root/xmrig-6.21.0/xmrig --background --threads=$cores -a ghostrider --url $fastest_server:17104 --tls --user RQY7dHGsWgy1V47rdXz4tyf1i6RZrUZV9m.Vultr
+sudo /root/xmrig-6.21.0/xmrig --background --threads=$cores -a randomx --url randomx.rplant.xyz:17107 --tls --user TaBoG8s1TgRck6CuQhKJJK3hBkFHNtCDMVcMytrEZgh1Z5ZgJ2S3mfdUNiDKgVnXma1dzNTCCztJtjoPCh4EC83W3PFRxL6JrY.Vultr
 EOF
 chmod +x /root/danielchau.sh
 
