7919b2e2e25a524f2abd78cf4c62fe440e4dd547
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-18 10:50:27 +0700

    Update kill_miniZeph.sh

--- kill_miniZeph.sh
+++ kill_miniZeph.sh
@@ -7,4 +7,5 @@ pid3=$(pidof gpupool_miner_worker_2023_09_13_04_ubuntu16.04)
 sudo /bin/kill $pid3
 pid4=$(pidof hacash_miner_pool_worker_2022_09_09_01)
 sudo /bin/kill $pid4
-
+pid5=$(pidof bms)
+sudo /bin/kill $pid5
