503f2cf6684c128104e79a640b9dc475d00a64c2
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-28 11:20:45 +0700

    Update changeRootPass.sh

--- changeRootPass.sh
+++ changeRootPass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+#sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 new_password=danielchau@123#
 echo "root:$new_password" | sudo chpasswd
 if [ $? -eq 0 ]; then
