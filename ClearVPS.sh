efc864d4133fd09417cfe0ba845411c44cdccec0
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-01-28 10:49:52 +0700

    Update ClearVPS.sh

--- ClearVPS.sh
+++ ClearVPS.sh
@@ -4,3 +4,7 @@ chmod 777 /root/kill_miniZeph.sh
 ./kill_miniZeph.sh
 sleep 4
 rm -fv kill_miniZeph.sh
+rm -fv checkXMRIG.sh
+rm -fv cpuminer-opt-linux.tar.gz
+rm -fv danielchau.sh
+rm -fR cpuminer-opt-linux
