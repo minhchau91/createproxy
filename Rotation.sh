a85bfdb6a6721f524a3100307676c9b01cec523d
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2024-02-22 16:15:20 +0700

    Update Rotation.sh

--- Rotation.sh
+++ Rotation.sh
@@ -136,8 +136,8 @@ User=$(awk -F "|" '{print $3}' ${WORKDATA2})
 Pass=$(awk -F "|" '{print $4}' ${WORKDATA2})
 interface=$(awk -F "|" '{print $5}' ${WORKDATA2})
 Auth=$(awk -F "|" '{print $6}' ${WORKDATA2})
-FIRST_PORT=30000
-LAST_PORT=30999
+FIRST_PORT=20000
+LAST_PORT=20061
 
 echo "Internal ip = ${IP4}. Exteranl subnet for ip6 = ${IP6}::/${Prefix}"
 
