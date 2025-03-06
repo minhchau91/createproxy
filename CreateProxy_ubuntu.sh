4d5d11c1b51e931de45c66c3c7bf877d46cbc7b8
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2025-03-06 10:28:17 +0700

    Update CreateProxy_ubuntu.sh

--- CreateProxy_ubuntu.sh
+++ CreateProxy_ubuntu.sh
@@ -183,10 +183,10 @@ fi
 echo "Detected your active interface: $checkinterface"
 
 Auth=none
-User=phamson
-Pass=phamson2025
+User=drt
+Pass=drt2025
 FIRST_PORT=20000
-LAST_PORT=20099
+LAST_PORT=20249
 
 rm -fv $WORKDIR/ipv6-subnet.txt
 cat >>$WORKDIR/ipv6-subnet.txt <<EOF
@@ -230,7 +230,7 @@ chmod 777 /root/changeipv6.sh
 
 cat /dev/null > /var/spool/cron/crontabs/root
 crontab -r
-echo '*/15 * * * * /root/Rotation.sh > /root/Rotation_log.txt' | crontab -
+echo '*/5 * * * * /root/Rotation.sh > /root/Rotation_log.txt' | crontab -
 
 #Add Cronjob
 #cat >>/var/spool/cron/crontabs/root<<EOF
